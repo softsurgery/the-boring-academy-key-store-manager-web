258ef590713bc8bd9c06b1c5e8add7181487b234
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2024-06-29 21:37:04 +0100

    feat(implement keys page)

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { menuItems } from "./constants/MenuItems";
 import { cn } from "@/lib/cn";
-import { Link,useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface SidebarProps {
   className?: string;
@@ -10,22 +10,26 @@ export default function Sidebar({ className }: SidebarProps) {
   const location = useLocation();
 
   return (
-    <nav className={cn(className,"grid items-start px-2 text-sm font-medium lg:px-4")}>
+    <nav
+      className={cn(
+        className,
+        "grid items-start px-2 text-sm font-medium lg:px-4"
+      )}
+    >
       {menuItems.map(({ label, icon, href }) => (
-        <div>
         <Link
           key={label}
           to={href}
           className={cn(
             "flex items-center gap-3 rounded-lg px-3 py-2 transition-all",
             location.pathname === href
-            ? "bg-muted text-foreground"
-            : "text-muted-foreground hover:text-foreground"
+              ? "bg-muted text-foreground"
+              : "text-muted-foreground hover:text-foreground"
           )}
         >
           {icon}
           {label}
-        </Link></div>
+        </Link>
       ))}
     </nav>
   );
